44a4f161b515df861df8a92c7dba4bd1a6642cd6
Nelson Diaz <nelsondiazrojas0130@gmail.com>
2018-07-05 23:40:46 -0500

    Add relationships between models

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use App\Product;
+use App\ProductImage;
+use App\Category;
 
 class ProductsTableSeeder extends Seeder
 {
@@ -12,6 +14,28 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
+        /*factory(Category::class, 5)->create();
         factory(Product::class, 100)->create();
+        factory(ProductImage::class, 200)->create();*/
+
+        $categories = factory(Category::class, 5)->create();
+
+        $categories->each(function ($category) {
+
+            $products = factory(Product::class, 20)->make();
+            $category->products()->saveMany($products);
+
+            $products->each(function ($p) {
+
+                $images = factory(ProductImage::class, 5)->make();
+                $p->images()->saveMany($images);
+            });
+        });
+
+        /*$users = factory(App\User::class, 3)
+           ->create()
+           ->each(function ($u) {
+                $u->posts()->save(factory(App\Post::class)->make());
+            });*/
     }
 }
